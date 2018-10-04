8c8535452a4a86422d490e99ce5cd2b5801e5fb5
Leora Wenger <web@leoraw.com>
2018-10-04 12:49:33 -0400

    button and card - button will have event

--- src/components/button.js
+++ src/components/button.js
@@ -1,1 +1,8 @@
 import React, { Component } from 'react';
+
+class Button extends Component {
+    //this will deal with a onClick on a button event
+    //then we will need to save the Selected Button
+}
+
+export default Button;
