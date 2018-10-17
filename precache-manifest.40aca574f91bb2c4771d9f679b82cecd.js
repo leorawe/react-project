eaf8bce475d3d4bf73284fc37e39229758633081
Leora Wenger <web@leoraw.com>
2018-10-17 08:25:30 -0400

    Updates

---
+++ precache-manifest.40aca574f91bb2c4771d9f679b82cecd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a54ebb76cbb06400fd1",
+    "url": "/react-project/static/js/runtime~main.0a54ebb7.js"
+  },
+  {
+    "revision": "bb210e13717229f2a5d8",
+    "url": "/react-project/static/js/main.bb210e13.chunk.js"
+  },
+  {
+    "revision": "9d14c0b9c85cfbe85ccc",
+    "url": "/react-project/static/js/1.9d14c0b9.chunk.js"
+  },
+  {
+    "revision": "bb210e13717229f2a5d8",
+    "url": "/react-project/static/css/main.47bdf974.chunk.css"
+  },
+  {
+    "revision": "3fe7032eb85747e9b3ed8bd19a20e641",
+    "url": "/react-project/index.html"
+  }
+];
